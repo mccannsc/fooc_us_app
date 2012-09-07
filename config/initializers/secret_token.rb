36e8a1b21b897b749812ce4657fec5e5a17088f3
mccannsc <mccannsc@gmail.com>
2012-09-07 20:45:33 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoocUsApp::Application.config.secret_token = '9c02ee3ee45cd2b6fef904aa05c584c79895fb9911c02e748ea76c29a4f87e3d952576aa66a517e06c347b1d786c31529c0a2071686401725a25bef266351a01'
