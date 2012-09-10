a74997494f0b29e03553f1b15f95e30b6c69e0f1
mccannsc <mccannsc@gmail.com>
2012-09-10 18:50:16 +0100

    Added a hack start and stop button

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ FoocUsApp::Application.routes.draw do
   match '/signin',        to: 'sessions#new'
   match '/signout',       to: 'sessions#destroy', via: :delete
   
+  
   match '/about',         to: 'static_pages#about'
   match '/contact',       to: 'static_pages#contact'
   match '/team',          to: 'static_pages#team'
