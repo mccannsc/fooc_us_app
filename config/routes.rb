3a45eb00c6418c9994aa4cc6579b899c9ea7bb36
Simon McCann <mccannsc@gmail.com>
2012-11-14 22:22:01 +0000

    Revert "delete immed"
    This reverts commit a2a8093f59c76a3f1f4d47afed41ec08ac47e2d0.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 FoocUsApp::Application.routes.draw do
 
-  
-  devise_for :admins
-  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
-
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :tasks, only: [:create, :destroy]
