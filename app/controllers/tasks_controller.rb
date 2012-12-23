399e7acb7c7e1d5faca0465195df6221d1db9914
Simon McCann <mccannsc@gmail.com>
2012-12-23 18:40:38 +0000

    Updated the layout for the time.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,7 @@ class TasksController < ApplicationController
       flash[:success] = "Task created. Get going!"
       redirect_to user_path(@user)
     else
-      flash[:error] = "Please fill in your task."
+      flash[:error] = "Check your time is a number and you have completed the task."
       redirect_to root_path     
      # render 'static_pages/home'
     end
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,7 @@ class Task < ActiveRecord::Base
   
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
-  validates :time, presence: true
+  validates :time, presence: true, :numericality => true
   
   default_scope order: 'tasks.created_at DESC'
   
